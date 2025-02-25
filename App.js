b353fe20e814c6e5bef510828c9b131b57d4a04a
ShadowNinja909 <175156558+TerrellAW@users.noreply.github.com>
2025-02-25 16:58:44 -0700

    Uploaded program files

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native";
+
+import Physics from "./components/Physics";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+          flex: 1,
+        }}
+    >     
+      <Physics />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Physics.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useRef } from "react";
+import { Animated, View } from "react-native";
+
+export default function Physics() {
+  // useRef ensures values are not reset each render
+
+  // Player controller
+  const positionXRef = useRef(new Animated.Value(0)).current;
+  const positionYRef = useRef(new Animated.Value(0)).current;
+
+  // Frame reference
+  const frame = useRef(null);
+
+  // Physics variables
+  const gravity = 0.2;
+  let velocityXRef = useRef(0);
+  let velocityYRef = useRef(0);
+
+  // Create recursive update loop for physics
+  const Update = () => {
+    // Update physics
+
+    // Log velocity
+    console.log(velocityYRef.current);
+
+    // Get next position
+    const nextY = positionYRef._value + velocityYRef.current;
+
+    // Get floor position
+    const floorY = window.innerHeight - 150;
+
+    // Check if player is grounded
+    const isGrounded = positionYRef._value > floorY - 1;
+
+    // Floor collision (100 is floor height and 50 is player height)
+    if (isGrounded) {
+      // Bounce with 20% of energy
+      velocityYRef.current *= -0.2;
+
+      // Rest on floor
+      if (Math.abs(velocityYRef.current) < 0.5) {
+        velocityYRef.current = 0;
+        positionYRef.setValue(floorY - 1); // One pixel above floor
+        frame.current = requestAnimationFrame(Update); // Call next physics render, necessary due to return
+        return; // Exit early when grounded, prevents falling through floor
+      }
+    } else {
+      // Fall
+      velocityYRef.current += gravity;
+    }
+
+    positionYRef.setValue(nextY);
+
+    // Ensure frame-based updates
+    frame.current = requestAnimationFrame(Update);
+  };
+
+  useEffect(() => {
+    // Start physics loop
+    frame.current = requestAnimationFrame(Update);
+
+    // Clean up
+    return () => {
+      cancelAnimationFrame(frame.current);
+    };
+  }, []);
+
+  // Render graphics with Animated API
+  return (
+    <View
+      style={{
+        flex: 1,
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#202121",
+      }}
+    >
+      {/* Player */}
+      <Animated.View
+        style={{
+          position: "absolute",
+          width: 50,
+          height: 50,
+          backgroundColor: "#f0f0f0",
+          transform: [
+            { translateX: positionXRef },
+            { translateY: positionYRef },
+          ],
+        }}
+      />
+      {/* Floor */}
+      <View
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          height: 100,
+          backgroundColor: "#f0f0f0",
+        }}
+      />
+    </View>
+  );
+}
